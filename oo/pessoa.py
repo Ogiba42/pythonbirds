f4959a15ced60ccbf2d17389f544b7705b9cd923
Gilberto <gilbertorb@outlook.com.br>
2020-09-10 14:27:33 -0300

    criados atributos de istancia nome e idade.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -2,6 +2,16 @@ class Pessoa:
     def cumprimentar(self):
         return 'Olá'
 
+    def diga_oi(self):
+        return 'Oi galera'
+
+    def __init__(self, nome=None, idade=28):
+        self.idade = idade
+        self.nome = nome
+
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Giba')
     print(p.cumprimentar())
+    print(p.diga_oi())
+    print(p.nome)
+    print(p.idade)
